13ca03521dcebe09a615ffbbcde2146753abed2e
PAKIWASI <ullahwasi26@gmail.com>
2026-06-01 00:12:43 +0500

    autos start fix/ waybar fix

--- .config/hypr/autostart.lua
+++ .config/hypr/autostart.lua
@@ -5,10 +5,9 @@
 hl.on("hyprland.start", function ()
     hl.exec_cmd('mako')
     hl.exec_cmd('waybar')
-    hl.exec_cmd('brightnessctl set 40%')
-    -- TODO: not setting, even though brighness is set to 50%, maybe defer it to later?
-    hl.exec_cmd('bluetooth off')
-    -- hl.dsp.exec_cmd('bluetooth off')
+    -- hl.exec_cmd('brightnessctl set 40%')
+    hl.dsp.exec_cmd('brightnessctl set 40%')
+    hl.dsp.exec_cmd('bluetooth off')
     hl.exec_cmd('wl-paste --type text --watch cliphist store')
     hl.exec_cmd('wl-paste --type image --watch cliphist store')
     hl.exec_cmd('hypridle')
