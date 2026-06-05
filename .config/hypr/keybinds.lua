f402c7ae0008be24fa1f2497c7343a41dd92d651
PAKIWASI <ullahwasi26@gmail.com>
2026-06-05 19:55:42 +0500

    removed adblock.js from spicetify | new alias zsh

--- .config/hypr/keybinds.lua
+++ .config/hypr/keybinds.lua
@@ -43,7 +43,6 @@ hl.bind(mainMod .. " + L",  hl.dsp.focus({ direction = "right" }))
 hl.bind(mainMod .. " + K",  hl.dsp.focus({ direction = "up" }))
 hl.bind(mainMod .. " + J",  hl.dsp.focus({ direction = "down" }))
 
-
 -- Switch workspaces with mainMod + [0-9]
 -- Move active window to a workspace with mainMod + SHIFT + [0-9]
 for i = 1, 10 do
